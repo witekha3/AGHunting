6b5d0f53087373f52fbccce9bd21c20b758603f9
Witold <46688375+witekha3@users.noreply.github.com>
2019-03-14 00:36:15 +0100

    Added movement and some models

---
+++ Client/AGHunting/Assets/Scripts/Animat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animat : MonoBehaviour
+{
+    public Animator animator;
+    public float RunForward;
+    public float RunSide;
+    public float Jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunForward = Input.GetAxis("Vertical");
+        animator.SetFloat("RunForward", RunForward);
+
+        RunSide = Input.GetAxis("Horizontal");
+        animator.SetFloat("RunSide", RunSide);
+
+        Jump = Input.GetAxis("Jump");
+        animator.SetFloat("Jump", Jump);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          //  animator.Play("Jump", 1, 0f);
+        }
+    }
+}
