fdda791e8afdb0ea19921da2a2701c9f635ebb19
Witold <46688375+witekha3@users.noreply.github.com>
2019-03-16 17:52:54 +0100

    - Added Enemy and working on attack

---
+++ Client/AGHunting/Assets/Scripts/Attack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Rigidbody player;
+    public Rigidbody Enemy;
+    public LayerMask layer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+        Enemy = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(Physics.CheckCapsule(player.position, Enemy.position, layer));  // poprawa w chuj
+        
+        
+    }
+}
---
+++ Client/AGHunting/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+    public float speed = 10f;
+    public float jumpForce = 8f;
+    CapsuleCollider col;
+
+    public LayerMask groundLayers;
+
+    //Animators
+    public Animator animat;
+    public float RunForward;
+
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        col = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+        animat = this.gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get Axis where to move
+        float MoveX = Input.GetAxisRaw("Horizontal");
+        float MoveZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 MoveHorizontal = transform.right * MoveX;
+        Vector3 MoveVertical = transform.forward * MoveZ;
+
+        //Create velocity
+        Vector3 Velocity = (MoveHorizontal + MoveVertical).normalized * speed;
+
+        //Movement
+        if (Velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + Velocity * Time.deltaTime);
+            animat.SetBool("IsRun", true);
+
+        }
+        else {
+            animat.SetBool("IsRun", false);
+
+        }
+
+        // jumping
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            animat.SetTrigger("Jump");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            animat.SetBool("Attack_1",true);
+        }
+        else
+        {
+            animat.SetBool("Attack_1",false);
+        }
+
+
+        bool IsGrounded()
+        {
+            return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+        }
+    }
+}
