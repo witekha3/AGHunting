39dbd3dbd653f517f07a3e3b675fc81e55d719e6
Witold <46688375+witekha3@users.noreply.github.com>
2019-03-18 00:16:17 +0100

    Some Fixes

--- Client/AGHunting/Assets/Scripts/Attack.cs
+++ Client/AGHunting/Assets/Scripts/Attack.cs
@@ -7,21 +7,26 @@ public class Attack : MonoBehaviour
     public Rigidbody player;
     public Rigidbody Enemy;
     public LayerMask layer;
-
-
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody>();
-        Enemy = GetComponent<Rigidbody>();
+      //  Enemy = GetComponent<Rigidbody>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Debug.Log(Physics.CheckCapsule(player.position, Enemy.position, layer));  // poprawa w chuj
+        float distance = Vector3.Distance(player.position, Enemy.position);
+        if (distance < 4f){
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Debug.Log("Atacked");
+            }
+        }
         
         
     }
